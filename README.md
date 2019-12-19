79611fac29ab3a1ae5d4d1ef42c3ce3f43d38f94
ahmetbcakici <ahmetbcakici@gmail.com>
2019-12-20 00:42:41 +0300

    initial

---
+++ README.md
@@ -0,0 +1,100 @@
+# About Package
+
+JsonDB is a small and open source project which is usable for JS developers. You can stay your DB in a json file easily with this package.
+
+# Usage
+
+## Installation
+
+`npm i js-json-db
+
+## Require
+
+```coffeescript
+const JsonDB = require("jsondb");
+const jDB = new JsonDB();
+```
+
+Now you can access package's functions but firstly you should generate a db and this function will be run just one time.
+
+## Database & Table Management
+
+### Generate Database
+
+```coffeescript
+const JsonDB = require("jsondb");
+const jDB = new JsonDB();
+
+jDB.generatejDB("sample_db");
+```
+
+We have a database anymore! We also should generate tables in this database. But never forget that you should always use jDB.usejDB function while working on your database. This function always should be active.
+
+### Generate Table
+
+```coffeescript
+jDB.usejDB("sample_db");
+jDB.newTABLE("table_name",["column1","column2"...]);
+```
+
+### Drop Table
+
+```coffeescript
+jDB.usejDB("sample_db");
+jDB.dropTABLE("table_name");
+```
+
+You can specify column names in quotes as second parameter.
+
+## Usage Basic Functions
+
+### Insert
+
+```coffeescript
+//jDB.generatejDB("sample_db");
+jDB.usejDB("sample_db");
+
+jDB.newTABLE("student",["student_no","student_name","student_department"]);
+jDB.insert("student",[1,"John","Computer Science Department"]);
+```
+
+### Find && FindOne
+
+```coffeescript
+jDB.usejDB("sample_db");
+
+jDB.find("student",(data) => {
+    console.log(data);
+})
+
+#w/Filter :
+
+jDB.find("student",{"student_no":1,"student_name":"John"},(data) => {
+    console.log(data);
+})
+
+#Also you can findOne too with filter or without filter
+#You can use filter more on as below
+
+jDB.findOne("student", { "student_no": 1, "student_name": "John" }, (data) => {
+    console.log();
+})
+```
+
+### Update
+
+```coffeescript
+jDB.usejDB("sample_db");
+jDB.update("student", { "student_name": "John" });#This line changes every record student_name with John in student table because we did not use filter parameter as thirdy
+#w/Filter :
+jDB.update("student", { "student_name": "John" },{ "student_no": 1 });#student_name will change as John which student has number 1
+```
+
+### Delete
+
+```coffeescript
+jDB.usejDB("sample_db");
+jDB.delete("student");#Pretty similar with update.This line delete all records in student table
+#w/Filter :
+jDB.delete("student",{"student_no":1});
+```
---
+++ jsonDB.js
@@ -0,0 +1,157 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = class jsonDB {
+        constructor() {
+            if (!fs.existsSync("./jDB"))
+                fs.mkdirSync("./jDB");
+        }
+
+        generatejDB(db_name) {
+            fs.writeFile(`./jDB/${db_name}.json`, JSON.stringify({}), (err) => {
+                if (err) throw err;
+            });
+        }
+
+        usejDB(db_name) {
+            this.db_name = db_name;
+            this.db = require(`${path.dirname(require.main.filename)}/jDB/${db_name}.json`);
+        }
+
+        newTABLE(table, array) {
+            this.db = {...this.db,
+                [table]: [{
+                    [array[0]]: null,
+                    [array[1]]: null,
+                    [array[2]]: null,
+                    [array[3]]: null,
+                    [array[4]]: null,
+                    [array[5]]: null,
+                    [array[6]]: null,
+                    [array[7]]: null,
+                    [array[8]]: null,
+                    [array[9]]: null
+                }]
+            }
+            this.saveChanges();
+        }
+
+        dropTABLE(table) {
+            delete this.db[table];
+            this.saveChanges();
+        }
+
+        insert(table, array) {
+            let keynames = [];
+            let counter = 0;
+            this.db[table].slice(0, 1).map(docs => {
+                Object.keys(docs).map(data => {
+                    keynames[counter] = data;
+                    counter++;
+                })
+            })
+
+            try {
+                this.db[table].push({
+                    [keynames[0]]: array[0],
+                    [keynames[1]]: array[1],
+                    [keynames[2]]: array[2],
+                    [keynames[3]]: array[3],
+                    [keynames[4]]: array[4],
+                    [keynames[5]]: array[5],
+                    [keynames[6]]: array[6],
+                    [keynames[7]]: array[7],
+                    [keynames[8]]: array[8],
+                    [keynames[9]]: array[9],
+                })
+                this.saveChanges();
+            } catch (e) { console.log(`${table} table not found`); }
+        }
+
+        find(table, filter, callback) {
+            try {
+                if (typeof filter == "function") {
+                    filter(this.db[table].slice(1)); //callback
+                } else {
+                    this.db[table].map(data => {
+                        if (data[Object.keys(filter)[0]] == filter[Object.keys(filter)[0]] && data[Object.keys(filter)[1]] == filter[Object.keys(filter)[1]] && data[Object.keys(filter)[2]] == filter[Object.keys(filter)[2]] && data[Object.keys(filter)[3]] == filter[Object.keys(filter)[3]] && data[Object.keys(filter)[4]] == filter[Object.keys(filter)[4]] && data[Object.keys(filter)[5]] == filter[Object.keys(filter)[5]] && data[Object.keys(filter)[6]] == filter[Object.keys(filter)[6]] && data[Object.keys(filter)[7]] == filter[Object.keys(filter)[7]] && data[Object.keys(filter)[8]] == filter[Object.keys(filter)[8]] && data[Object.keys(filter)[9]] == filter[Object.keys(filter)[9]]) {
+                            if (typeof callback == "function")
+                                callback(data);
+                        }
+                    })
+                }
+            } catch (e) { console.log(`${table} table not found`); }
+        }
+
+        findOne(table, filter, callback) {
+            try {
+                if (typeof filter == "function") {
+                    filter(this.db[table].slice(1, 2));
+                } else {
+                    this.db[table].some(data => {
+                        if (data[Object.keys(filter)[0]] == filter[Object.keys(filter)[0]] && data[Object.keys(filter)[1]] == filter[Object.keys(filter)[1]] && data[Object.keys(filter)[2]] == filter[Object.keys(filter)[2]] && data[Object.keys(filter)[3]] == filter[Object.keys(filter)[3]] && data[Object.keys(filter)[4]] == filter[Object.keys(filter)[4]] && data[Object.keys(filter)[5]] == filter[Object.keys(filter)[5]] && data[Object.keys(filter)[6]] == filter[Object.keys(filter)[6]] && data[Object.keys(filter)[7]] == filter[Object.keys(filter)[7]] && data[Object.keys(filter)[8]] == filter[Object.keys(filter)[8]] && data[Object.keys(filter)[9]] == filter[Object.keys(filter)[9]]) {
+                            if (typeof callback == "function")
+                                callback(data);
+                            return true;
+                        }
+                    })
+                }
+            } catch (e) { console.log(`${table} table not found`); }
+        }
+
+        update(table, set, filter) {
+            try {
+                if (filter) {
+                    this.db[table].map(data => {
+                        if (data[Object.keys(filter)[0]] == filter[Object.keys(filter)[0]] && data[Object.keys(filter)[1]] == filter[Object.keys(filter)[1]] && data[Object.keys(filter)[2]] == filter[Object.keys(filter)[2]] && data[Object.keys(filter)[3]] == filter[Object.keys(filter)[3]] && data[Object.keys(filter)[4]] == filter[Object.keys(filter)[4]] && data[Object.keys(filter)[5]] == filter[Object.keys(filter)[5]] && data[Object.keys(filter)[6]] == filter[Object.keys(filter)[6]] && data[Object.keys(filter)[7]] == filter[Object.keys(filter)[7]] && data[Object.keys(filter)[8]] == filter[Object.keys(filter)[8]] && data[Object.keys(filter)[9]] == filter[Object.keys(filter)[9]])
+                            data[Object.keys(set)[0]] = set[Object.keys(set)[0]];
+                    })
+                } else {
+                    this.db[table].map(data => {
+                        data[Object.keys(set)[0]] = set[Object.keys(set)[0]];
+                    })
+                }
+                this.saveChanges();
+            } catch (e) { console.log(`${table} table not found`); }
+        }
+
+        delete(table, filter) {
+            try {
+                let counter = 1;
+
+                if (filter) {
+                    counter = 0;
+                    this.db[table].map(data => {
+                        if (data[Object.keys(filter)[0]] == filter[Object.keys(filter)[0]])
+                            delete this.db[table][counter];
+                        counter++;
+                    })
+                } else {
+                    this.db[table].map(data => {
+                        delete this.db[table][counter];
+                        counter++;
+                    })
+                }
+
+                let temp_len = this.db[table].length;
+                for (let i = 0; i < temp_len; i++) {
+                    for (let i = 0; i < this.db[table].length; i++) {
+                        if (!this.db[table][i]) {
+                            this.db[table].splice(this.db[table].indexOf(i), 1);
+                        }
+                    }
+                }
+                this.saveChanges();
+            } catch (e) { console.log(`${table} table not found`); }
+        }
+
+        saveChanges() {
+            fs.writeFile(`./jDB/${this.db_name}.json`, JSON.stringify(this.db), function(err) {
+                if (err) throw err;
+            });
+        }
+    }
+    //eklenebielcekler : AUTO INCREMENT - MULTIPLE INSERT - BACKUP SYSTEM - ADVANCED FILTER QUERIES LIKE OR OPERATÖRLER > < != 
+
+//uyarılar : databaseinizin açıkken işlem yapmayın
+//geneate db te başına kullanılacak yoksa hata alırsınız
